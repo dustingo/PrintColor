63865be024f29348d5ab79aec3fb4fccb46b0fbc
dustingo <514838728@qq.com>
2020-05-28 15:26:07 +0800

    update and you can use this module on python2.x

--- setup.py
+++ setup.py
@@ -2,18 +2,12 @@
 # -*- coding:utf-8 -*-
 from __future__ import print_function
 from setuptools import setup, find_packages
-import platform
-pyversion = platform.python_version()
-if pyversion.startswith("3"):
-    py_modules = ["printcolor"]
-else:
-    py_modules = ["printcolor2"]
 
 with open("README.md",'r') as f:
     long_description = f.read()
 
 setup(name = "printcolor",
-    version = "1.1.2",
+    version = "1.1.3",
     description = "print colorful string",
     author = "zack",
     author_email = "514838728@qq.com",
@@ -22,6 +16,6 @@ setup(name = "printcolor",
     url = "https://github.com/dustingo/PrintColor.git",
     packages = find_packages(),
     install_requires = [],
-    py_modules = py_modules,
+    py_modules = ["printcolor","printcolor2"],
     platforms = ["linux"],
 )
