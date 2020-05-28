bf1f412c6e5195c97e97243badd340d6c7466d16
dustingo <514838728@qq.com>
2020-05-28 11:34:18 +0800

    update setup.py file

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 from __future__ import print_function
 from setuptools import setup, find_packages
 
 with open("README.md",'r',encoding='utf-8') as f:
     long_description = f.read()
 
-setup = (
-    name='pycolor',
-    author='zack',
-    version='1.0',
-    license='MIT',
-    description='print colorful string',
-    url = "",
+setup(name = "printcolor",
+    version = "1.0",
+    description = "print colorful string",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    author = "zack",
+    author_email = "514838728@qq.com",
+    url = "https://github.com/dustingo/PrintColor.git",
     packages = find_packages(),
-    platform = "any",
-    install_requires = []
+    install_requires = [],
+    py_modules=["printcolor"],
 )
